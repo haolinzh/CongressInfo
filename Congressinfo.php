22bc37bebdc357f833f3a1bbfa6faa8836c3dec3
haolinzh <haolinzh@usc.edu>
2016-10-13 23:03:16 -0700

    Need to implement View Detail

--- Congressinfo.php
+++ Congressinfo.php
@@ -1,75 +1,117 @@
 <html>
 <head>
     <style>
-        td{
-            text-align: center;
-        }
-        .tab{
-            border: 2px solid;
-            border-collapse:collapse;
-
-        }
-         .tab th,.tab td {
-            border: 2px solid;
+        td {
             text-align: center;
         }
 
-        #init{
+        .tab {
+            border: 2px solid;
+            border-collapse: collapse;
+
+        }
+
+        .tab th, .tab td {
+            border: 2px solid;
+            text-align: center;
+            padding: 5px 15px;
+
+        }
+
+        #init {
             border: 1px solid;
-            width: 25%;
+            width: 28%;
         }
-        </style>
+    </style>
 </head>
 <body>
 <?php
 $database = $chamber = $keyword = $submit = "";
-$state['alabama']="AL";$state['alaska']="AK";$state['arizona']="AZ";$state['arkansas']="AR";
-$state['california']="CA";$state['colorado']="CO";$state['connecticut']="CT";$state['delaware']="DE";
-$state['district of columbia']="DC";$state['florida']="FL";$state['georgia']="GA";$state['hawaii']="HI";
-$state['Idaho']="ID";$state['Illinois']="IL";$state['Indiana']="IN";$state['Iowa']="IA";
-$state['kansas']="KS";$state['kentucky']="KY";$state['louisiana']="LA";$state['maine']="ME";
-$state['maryland']="MD";$state['massachusetts']="MA";$state['michigan']="MI";$state['minnesota']="MN";
-$state['mississippi']="MS";$state['missouri']="MO";$state['montana']="MT";$state['nebraska']="NE";
-$state['nevada']="NV";$state['new hampshire']="NH";$state['new jersey']="NJ";$state['new mexico']="NM";
-$state['new york']="NY";$state['north carolina']="NC";$state['north dakota']="ND";$state['ohio']="OH";
-$state['oklahoma']="OK";$state['oregon']="OR";$state['pennsylvania']="PA";$state['rhode island']="RI";$state['south carolina']="SC";
-$state['south dakota']="SD";$state['tennessee']="TN";$state['texas']="TX";$state['utah']="UT";$state['vermont']="VT";
-$state['virginia']="VA";$state['washington']="WA";$state['west virginia']="WV";$state['wisconsin']="WI";
-$state['wyoming']="WY";
+$state['alabama'] = "AL";
+$state['alaska'] = "AK";
+$state['arizona'] = "AZ";
+$state['arkansas'] = "AR";
+$state['california'] = "CA";
+$state['colorado'] = "CO";
+$state['connecticut'] = "CT";
+$state['delaware'] = "DE";
+$state['district of columbia'] = "DC";
+$state['florida'] = "FL";
+$state['georgia'] = "GA";
+$state['hawaii'] = "HI";
+$state['Idaho'] = "ID";
+$state['Illinois'] = "IL";
+$state['Indiana'] = "IN";
+$state['Iowa'] = "IA";
+$state['kansas'] = "KS";
+$state['kentucky'] = "KY";
+$state['louisiana'] = "LA";
+$state['maine'] = "ME";
+$state['maryland'] = "MD";
+$state['massachusetts'] = "MA";
+$state['michigan'] = "MI";
+$state['minnesota'] = "MN";
+$state['mississippi'] = "MS";
+$state['missouri'] = "MO";
+$state['montana'] = "MT";
+$state['nebraska'] = "NE";
+$state['nevada'] = "NV";
+$state['new hampshire'] = "NH";
+$state['new jersey'] = "NJ";
+$state['new mexico'] = "NM";
+$state['new york'] = "NY";
+$state['north carolina'] = "NC";
+$state['north dakota'] = "ND";
+$state['ohio'] = "OH";
+$state['oklahoma'] = "OK";
+$state['oregon'] = "OR";
+$state['pennsylvania'] = "PA";
+$state['rhode island'] = "RI";
+$state['south carolina'] = "SC";
+$state['south dakota'] = "SD";
+$state['tennessee'] = "TN";
+$state['texas'] = "TX";
+$state['utah'] = "UT";
+$state['vermont'] = "VT";
+$state['virginia'] = "VA";
+$state['washington'] = "WA";
+$state['west virginia'] = "WV";
+$state['wisconsin'] = "WI";
+$state['wyoming'] = "WY";
+$PREFIX = 'http://congress.api.sunlightfoundation.com/';
 
-
-if ($_SERVER["REQUEST_METHOD"] == "GET"):
-    $database = $_GET["Database"];
-    $chamber = $_GET["Chamber"];
-    $keyword = $_GET["Keyword"];
-    $submit = $_GET["submit"];
+if ($_SERVER["REQUEST_METHOD"] == "POST"):
+    $database = $_POST["Database"];
+    $chamber = $_POST["Chamber"];
+    $keyword = $_POST["Keyword"];
+    $submit = $_POST["submit"];
 
 endif;
 ?>
 <script type="text/javascript">
-   function oc() {
-       if(document.getElementById("db").value=='legislators') {
-           document.getElementById("ky").innerHTML="State/Representative*"
-       }
-       if(document.getElementById("db").value=='committees') {
-           document.getElementById("ky").innerHTML="Committee ID*"
-       }
-       if(document.getElementById("db").value=='bills') {
-           document.getElementById("ky").innerHTML="Bill ID*"
-       }
-       if(document.getElementById("db").value=='amendments') {
-           document.getElementById("ky").innerHTML="Amendment ID*"
-       }
-   }
+    function oc() {
+        if (document.getElementById("db").value == 'legislators') {
+            document.getElementById("ky").innerHTML = "State/Representative*"
+        }
+        if (document.getElementById("db").value == 'committees') {
+            document.getElementById("ky").innerHTML = "Committee ID*"
+        }
+        if (document.getElementById("db").value == 'bills') {
+            document.getElementById("ky").innerHTML = "Bill ID*"
+        }
+        if (document.getElementById("db").value == 'amendments') {
+            document.getElementById("ky").innerHTML = "Amendment ID*"
+        }
+    }
 
 </script>
 <div align="center">
     <h2>Congrss Information Search</h2>
-    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" id="congressinfo" method="get" >
-        <div id="init" >
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" id="congressinfo" method="POST">
+        <div id="init">
             <table border="0">
                 <tr>
-                    <td>Congress DataBase</td>
+                    <td>Congress Database</td>
                     <td>
                         <select name="Database" id="db" onchange="oc()">
                             <option>Select your option</option>
@@ -109,7 +151,7 @@ endif;
                     <td></td>
                     <td>
                         <input type="submit" name="submit" value="Search">
-                        <input type="button" value="Reset" >
+                        <input type="button" value="Reset">
                     </td>
                 </tr>
             </table>
@@ -120,41 +162,56 @@ endif;
 </div>
 
 
-<?php if ( $database == "legislators"):
+<?php if ($database == "legislators"):
+    $lkeyword = strtolower($keyword);
+    $keyinstate = array_keys($state);
 
+    if (in_array($lkeyword, $keyinstate)) {
 
-    $PREFIX = 'http://congress.api.sunlightfoundation.com/';
-    $keyword=strtolower($keyword);
-    $keyword=$state[$keyword];
+        $lkeyword = $state[$lkeyword];
 
-    $context = $database . '?chamber=' . $chamber . '&state=' . $keyword . '&apikey=4acd972a599843bd93ea4dba171a483f';
-    $url = $PREFIX . $context;
+        $context = $database . '?chamber=' . $chamber . '&state=' . $lkeyword . '&apikey=4acd972a599843bd93ea4dba171a483f';
+        $url = $PREFIX . $context;
 
-    $html = file_get_contents($url);
-    $res = json_decode($html);
+        $html = file_get_contents($url);
+        $res = json_decode($html);
 
-    echo "<br>";
-    $cout = count($res->results);
+        echo "<br>";
+        $cout = count($res->results);
 
-    echo "<table class='tab' align='center'><tr><th><strong>Name</strong></th><th><strong>State</strong></th><th><strong>Chamber</strong></th><th><strong>Detail</strong></th></tr> ";
-    for ($i = 0; $i < $cout; $i++) {
-        $name = $res->results[$i]->first_name . " " . $res->results[$i]->middle_name . " " . $res->results[$i]->last_name;
-        $state = $res->results[$i]->state_name;
-        $chamber = $res->results[$i]->chamber;
-        echo "<tr><td>$name</td><td>$state</td><td>$chamber</td><td><a href=''>View Details</a></td></tr>";
+        echo "<table class='tab' align='center'><tr><th><strong>Name</strong></th><th><strong>State</strong></th><th><strong>Chamber</strong></th><th><strong>Detail</strong></th></tr> ";
+        for ($i = 0; $i < $cout; $i++) {
+            $name = $res->results[$i]->first_name . " " . $res->results[$i]->middle_name . " " . $res->results[$i]->last_name;
+            $state = $res->results[$i]->state_name;
+            $chamber = $res->results[$i]->chamber;
+            echo "<tr><td>$name</td><td>$state</td><td>$chamber</td><td><a href=''>View Details</a></td></tr>";
+        }
+        echo "</table>";
+    } else {
+        $context = $database . '?chamber=' . $chamber . '&query=' . $keyword . '&apikey=4acd972a599843bd93ea4dba171a483f';
+        $url = $PREFIX . $context;
+
+        $html = file_get_contents($url);
+        $res = json_decode($html);
+
+        echo "<br>";
+        $cout = count($res->results);
+
+        echo "<table class='tab' align='center'><tr><th><strong>Name</strong></th><th><strong>State</strong></th><th><strong>Chamber</strong></th><th><strong>Detail</strong></th></tr> ";
+        for ($i = 0; $i < $cout; $i++) {
+            $name = $res->results[$i]->first_name . " " . $res->results[$i]->middle_name . " " . $res->results[$i]->last_name;
+            $state = $res->results[$i]->state_name;
+            $chamber = $res->results[$i]->chamber;
+            echo "<tr><td>$name</td><td>$state</td><td>$chamber</td><td><a href=''>View Details</a></td></tr>";
+        }
+        echo "</table>";
     }
-    echo "</table>";
-
 
 endif; ?>
 
-<?php if ( $database == "committees"):
+<?php if ($database == "committees"):
 
-
-    $PREFIX = 'http://congress.api.sunlightfoundation.com/';
-
-
-    $context = $database.'?committee_id='.$keyword.'&chamber='.$chamber.'&apikey=4acd972a599843bd93ea4dba171a483f';
+    $context = $database . '?committee_id=' . $keyword . '&chamber=' . $chamber . '&apikey=4acd972a599843bd93ea4dba171a483f';
     $url = $PREFIX . $context;
 
     $html = file_get_contents($url);
@@ -174,12 +231,8 @@ endif; ?>
 
 endif; ?>
 
-<?php if ( $database == "bills"):
-
-
-    $PREFIX = 'http://congress.api.sunlightfoundation.com/';
-
-    $context = $database .'?bill_id=' . $keyword. '&chamber=' . $chamber . '&apikey=4acd972a599843bd93ea4dba171a483f';
+<?php if ($database == "bills"):
+    $context = $database . '?bill_id=' . $keyword . '&chamber=' . $chamber . '&apikey=4acd972a599843bd93ea4dba171a483f';
     $url = $PREFIX . $context;
 
 
@@ -189,9 +242,9 @@ endif; ?>
 
     echo "<table class='tab' align='center'><tr><th><strong>Bill ID</strong></th><th><strong>Short Title</strong></th><th><strong>Chamber/strong></th><th><strong>View Detail</strong></th></tr> ";
     for ($i = 0; $i < $cout; $i++) {
-        $billid=$res->results[$i]->bill_id;
-        $shorttitle=$res->results[$i]->short_title;
-        $chamber=$res->results[$i]->chamber;
+        $billid = $res->results[$i]->bill_id;
+        $shorttitle = $res->results[$i]->short_title;
+        $chamber = $res->results[$i]->chamber;
         echo "<tr><td>$billid</td><td>$shorttitle</td><td>$chamber</td><td><a href=''>View Details</a></td></tr>";
     }
     echo "</table>";
@@ -199,12 +252,9 @@ endif; ?>
 
 endif; ?>
 
-<?php if ( $database == "amendments"):
+<?php if ($database == "amendments"):
 
-
-    $PREFIX = 'http://congress.api.sunlightfoundation.com/';
-
-    $context = $database .'?amendment_id=' . $keyword. '&chamber=' . $chamber . '&apikey=4acd972a599843bd93ea4dba171a483f';
+    $context = $database . '?amendment_id=' . $keyword . '&chamber=' . $chamber . '&apikey=4acd972a599843bd93ea4dba171a483f';
     $url = $PREFIX . $context;
 
     $html = file_get_contents($url);
@@ -213,10 +263,10 @@ endif; ?>
 
     echo "<table  class='tab' align='center'><tr><th>Amendment ID</th><th>Amendment Type</th><th>Chamber</th><th>Introduced on</th></tr> ";
     for ($i = 0; $i < $cout; $i++) {
-        $amid=$res->results[$i]->amendment_id;
-        $amtype=$res->results[$i]->amendment_type;
-        $chamber=$res->results[$i]->chamber;
-        $intro=$res->results[$i]->introduced_on;
+        $amid = $res->results[$i]->amendment_id;
+        $amtype = $res->results[$i]->amendment_type;
+        $chamber = $res->results[$i]->chamber;
+        $intro = $res->results[$i]->introduced_on;
         echo "<tr><td>$amid</td><td>$amtype</td><td>$chamber</td><td>$intro</td></tr>";
     }
     echo "</table>";
